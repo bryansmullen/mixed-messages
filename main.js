f3929744fdbc55dda4a1ec5d329cb4db655e4bba
Bryan Mullen <bryan@bryanmullen.me>
2021-03-02 00:29:20 +0000

    Implements Print Message Functionality

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-/* 
+/*
  *  MIT License
- *  
+ *
  *  Copyright (c) 2021 bryansmullen
- *  
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,15 +22,25 @@
  *  SOFTWARE.
  */
 
-module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {},
+// import data
+const { musicians, dontPlays, plays } = require("./_data.json");
+
+/**
+ * Print Message to Console
+ * @returns {void}
+ */
+const printMessage = () => {
+  // message components
+  const musician = musicians[Math.floor(Math.random() * musicians.length)];
+  const dontPlay = dontPlays[Math.floor(Math.random() * dontPlays.length)];
+  const play = plays[Math.floor(Math.random() * plays.length)];
+
+  //    construct message
+  const msg = `In the words of the jazz great, ${musician}, don't play ${dontPlay}, play ${play}`;
+
+  //    print message
+  console.log(msg);
 };
+
+// call function
+printMessage();
